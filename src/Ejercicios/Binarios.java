f3cc265c49063f893d6f1c01f8ba11f82c386e0c
Trebellar <ola.trebellar@gmail.com>
2024-04-19 16:26:13 +0200

    Se añade clase Binarios para los ejercicios. Y varios ficheros para su uso con los ejercicios

--- src/Ejercicios/Binarios.java
+++ src/Ejercicios/Binarios.java
@@ -2,10 +2,7 @@ package Ejercicios;
 
 
 import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * @author Nacho Limetre
@@ -22,15 +19,81 @@ import java.io.IOException;
 public class Binarios {
 
     public static void main(String[] args) {
+        // Array de bytes con el tamaño fichero origen
+        byte[] buffer;
+        // Frame_Response = new byte[];
 
-       // JOptionPane.showMessageDialog(new JFrame(), "Erro ao salvar arquivo", "mensaje ", JOptionPane.ERROR_MESSAGE);
+        File pathDefecto = new File(System.getProperty("user.home") + System.getProperty("file.separator") + "dev\\java\\tbl\\src\\ficheros");
+        File ficheroOrigen;
+        File ficheroDestino;
+
+        // Para seleccion de archivo via GUI
+        JFileChooser fc = new JFileChooser();
+        fc.setCurrentDirectory(pathDefecto);
+
+        fc.showDialog(new JFrame(), "Copiar");
+        ficheroOrigen = fc.getSelectedFile();
+        System.out.println(ficheroOrigen.getParentFile());
+        System.out.println(ficheroOrigen.getName());
+
+        fc.showSaveDialog(new JFrame());
+        ficheroDestino = fc.getSelectedFile();
+
+
+        System.out.println("Origen  -> " + ficheroOrigen);
+        System.out.println("Destino -> " + ficheroDestino);
+
+        try {
+            FileInputStream fis = new FileInputStream(ficheroOrigen);
+            FileOutputStream fos = new FileOutputStream(ficheroDestino);
+
+            // Lee el tamaño de los bytes
+            System.out.println("avariable -> " + fis.available());
+
+            int tamanoBuffer = fis.available();
+            buffer = new byte[tamanoBuffer];
+
+            // Lee el fichero de origen y lo almacena en el array
+            for (int i = 0; i < tamanoBuffer; i++) {
+                buffer[i] = (byte) fis.read();
+                System.out.println(buffer[i] + " - " + (char) buffer[i]);
+            }
+            fis.close();
+
+            // Lee el array y lo escribe en el fichero de destino
+            for (int i = 0; i < tamanoBuffer; i++) {
+                fos.write(buffer[i]);
+                System.out.println(buffer[i] + " - " + buffer[i]);
+            }
+            fos.close();
+
+
+        } catch (FileNotFoundException e) {
+            System.err.println("Error\n" + e.getMessage());
+            ;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //fc.showOpenDialog(new JFrame());
+
+
+        // JOptionPane.showMessageDialog(new JFrame(), "Erro ao salvar arquivo", "mensaje ", JOptionPane.ERROR_MESSAGE);
 
         // JFileChooser é a janela de seleção de arquivo
-        JFileChooser fc = new JFileChooser();
-       //fc.showSaveDialog(new JFrame());
-       fc.showOpenDialog(new JFrame());
+        //fc.showSaveDialog(new JFrame());
 
-        System.out.println(fc.getSelectedFile());
+
     }
+
+
+    private String obtenerExtensionArchivo(File fichero){
+        String extension ="";
+
+
+
+        return extension;
+    }
+
 
 }
