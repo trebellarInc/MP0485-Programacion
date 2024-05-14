439dacb8ecfde7f675492c7ac208208e07ea6553
Trebellar <ola.trebellar@gmail.com>
2024-05-14 18:49:54 +0200

    Pruebas Json

--- src/Ejercicios/Entrega2_UD09/Principal.java
+++ src/Ejercicios/Entrega2_UD09/Principal.java
@@ -1,11 +1,13 @@
 package Ejercicios.Entrega2_UD09;
 
+import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
+
+
 import java.io.*;
 import java.util.ArrayList;
 
 public class Principal {
-
-
     public static void main(String[] args) throws IOException {
         System.out.println("Inicio Entrega 02 - UD09");
 
@@ -21,35 +23,75 @@ public class Principal {
         listaClientes.add(c3);
 
 
-/*
-         for (Cliente c : listaClientes){
-             System.out.println(c.getSaldoCuenta());
-        c.show();
-         }
-*/
+        String ficheroDestinoRutaJson = "entrega2UD9_objetosInput.json";
+        String ficheroDestinoRutaXml = "entrega2UD9_objetosInput.xml";
 
-        String ficheroDestinoRuta = "entrega2UD9_objetosInput.dat";
-        File ficheroDestino = new File(ficheroDestinoRuta);
+        File ficheroDestino = new File(ficheroDestinoRutaXml);
         if (!ficheroDestino.exists()) ficheroDestino.createNewFile();
 
+        File ficheroDestinoJson = new File(ficheroDestinoRutaXml);
+        if (!ficheroDestinoJson.exists()) ficheroDestinoJson.createNewFile();
 
-
-
-    }
-
-    public static void salvarClientes (ArrayList <Cliente> listaClientes , String ficheroDestino){
-        FileInputStream fis = new FileInputStream(new File(ficheroDestino));
-
-
-
-        FileOutputStream fos = new FileOutputStream((new File(ficheroDestino));
-
-
-
-
-
+        salvarClientesXML(listaClientes, ficheroDestinoRutaXml);
+        leeClientesXML(ficheroDestinoRutaXml);
+        salvarClientesJson(listaClientes, ficheroDestinoRutaJson);
 
     }
 
+    public static void salvarClientesXML(ArrayList<Cliente> listaClientes, String ficheroDestino) {
+        XStream xstream = new XStream();
 
+        xstream.alias("DatosCliente", Cliente.class);
+
+        System.out.println(xstream.toXML(listaClientes));
+
+        try {
+            xstream.toXML(listaClientes, new FileOutputStream(ficheroDestino));
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Creado el fichero xml");
+    }
+
+    public static void leeClientesXML(String ficheroDestino) {
+        XStream xstream = new XStream();
+
+        xstream.allowTypes(new Class[]{Cliente.class});
+
+        xstream.alias("DatosCliente", Cliente.class);
+
+        ArrayList<Cliente> lista;
+        try {
+            lista = (ArrayList<Cliente>) xstream.fromXML(new FileInputStream(ficheroDestino));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        for (Cliente c : lista) {
+            System.out.println(c.getSaldoCuenta());
+            c.show();
+        }
+    }
+
+    public static void salvarClientesJson(ArrayList<Cliente> listaClientes, String ficheroDestino) {
+        Gson gson = new Gson();
+        String json = gson.toJson(listaClientes);
+
+
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new FileWriter(ficheroDestino));
+            System.out.println(json);
+            bw.write(json);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                bw.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+
+    }
 }
