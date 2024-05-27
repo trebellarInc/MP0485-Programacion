518e92e751d9132f34e709ba986da873564e1b4c
Trebellar <ola.trebellar@gmail.com>
2024-05-27 09:54:55 +0200

    Agregar un validador de fechas. Valida este formato YYYY-MM-DD Valida este formato DD-MM-AAAA

--- src/Utiles/Fechas.java
+++ src/Utiles/Fechas.java
@@ -1,20 +1,82 @@
 package Utiles;
 
+import Prog3Eval.rev.Captura;
+
+import java.time.DateTimeException;
 import java.time.LocalDate;
-import java.util.Locale;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Fechas {
 
+    private static int dia;
+
     public static void main(String[] args) {
+        /*
+        // Test del metodo FechaEntre
+
         LocalDate inicio = LocalDate.of(1982,1,1);
         LocalDate fin = LocalDate.of(1999,12,31);
         System.out.println(fechaEntre(inicio,fin));
+                 */
+
+        String entradaTeclado = Captura.cadena("Introduce un fecha ?");
+        LocalDate f = null;
+        f = captura(entradaTeclado);
+        System.out.println("La fecha devuelta es -> " +f);
 
     }
 
-    public static LocalDate fechaEntre(LocalDate fechaInicio, LocalDate fechaFinal) {
 
+    public static LocalDate captura(String entradaTeclao) {
+
+
+        LocalDate retorno = null;
+
+        int anio = 0;
+        int mes = 0;
+        int dia = 0;
+
+
+        // Valida este formato YYYY-MM-DD
+        String patronFecha = "\\d{4}-\\d{2}-\\d{2}";
+        // Valida este formato DD-MM-AAAA
+        String patronFecha2 = "\\d{2}-\\d{2}-\\d{4}";
+
+
+        if (entradaTeclao.matches(patronFecha)) {
+            System.out.println("El formato introducido es YYYY-MM-DD");
+            anio = Integer.parseInt(entradaTeclao.substring(0, 4));
+            mes = Integer.parseInt(entradaTeclao.substring(5, 7));
+            dia = Integer.parseInt(entradaTeclao.substring(8));
+            System.out.println(anio + " " + mes + " " + dia);
+
+        } else if (entradaTeclao.matches(patronFecha2)) {
+            System.out.println("El formato introducido es DD-MM-AAAA");
+            dia = Integer.parseInt(entradaTeclao.substring(0, 2));
+            mes = Integer.parseInt(entradaTeclao.substring(3, 5));
+            anio = Integer.parseInt(entradaTeclao.substring(6));
+            System.out.println(anio + " " + mes + " " + dia);
+
+        } else {
+           // System.out.println("El formato de fecha introducido no es valido");
+        }
+
+
+        try {
+            retorno = LocalDate.of(anio, mes, dia);
+        } catch (DateTimeException | NumberFormatException | StringIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+            System.out.println("El formato de fecha introducido no es valido");
+
+        }
+
+
+        return retorno;
+
+    }
+
+
+    public static LocalDate fechaEntre(LocalDate fechaInicio, LocalDate fechaFinal) {
         // Definir las fechas de inicio y fin
         //LocalDate inicio = LocalDate.of(fechaInicio);
         //LocalDate fin = LocalDate.of(fechaFinal);
@@ -33,7 +95,6 @@ public class Fechas {
 
         // Retornar la fecha generadada entre las fechas dadas.
         return fechaAleatoria;
-
     }
 
 
